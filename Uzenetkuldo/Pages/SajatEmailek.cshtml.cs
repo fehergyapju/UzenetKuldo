db81946e804208eb9ec5e0b286e8c953a9457317
Gyapjú Fehér <mate.szabolcska@gmail.com>
2025-06-14 18:03:05 +0200

    Hozzáadtuk a Saját Emailek funkciót.

---
+++ Uzenetkuldo/Pages/SajatEmailek.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Uzenetkuldo.Pages
+{
+    public class SajatEmailekModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
